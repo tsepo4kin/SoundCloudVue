58700067000468c97e76b6ee6ac9539ad22fc41e
Kirill <kupuk132@gmail.com>
2020-10-16 00:57:02 +0300

    create first components, setting eslint and prettier

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-footer>
+    <v-container>
+      <v-row>
+        footer
+      </v-row>
+    </v-container>
+  </v-footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-header>
+    <v-container>
+      <v-row>
+        header
+      </v-row>
+    </v-container>
+  </v-header>
+</template>
+
+<script>
+export default {
+
+  computed: {
+    menuItems() {
+      if (this.isAuth) {
+        return [
+          {
+            title: "Catalog",
+            path: "/catalog",
+            icon: "mdi-calendar-month"
+          },
+          {
+            title: "Library",
+            path: "/library",
+            icon: "mdi-calendar-month"
+          },
+          {
+            title: "Profile",
+            path: "/profile",
+            icon: "mdi-account-box"
+          }
+        ];
+      } else {
+        return [
+          {
+            title: "SignIn",
+            path: "/signIn",
+            icon: "mdi-account-arrow-right"
+          },
+          {
+            title: "SignUp",
+            path: "/signUp",
+            icon: "mdi-account-check"
+          }
+        ];
+      }
+    },
+    isAuth() {
+      return this.$store.getters.isAuth;
+    }
+  }
+};
+</script>
+
+<style></style>
