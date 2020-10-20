d079eb07cc73ef3cc4ff15361b271a799769fb68
Kirill <kupuk132@gmail.com>
2020-10-20 23:04:09 +0300

    create profile component

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>
-    profile
-  </div>
+  <v-row>
+    <v-col>
+      <v-card>
+        <v-card-title>LogIn with VK</v-card-title>
+        <v-divider></v-divider>
+        <v-btn>LogIn</v-btn>
+      </v-card>
+    </v-col>
+
+    <v-col>
+      <v-card>
+        <v-card-title>LogIn with SoundCloud</v-card-title>
+        <v-divider></v-divider>
+        <v-btn>LogIn</v-btn>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
