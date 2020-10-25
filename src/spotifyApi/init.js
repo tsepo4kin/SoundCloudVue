d6e65a7372bdc5216fa5b18c6df6010c8f211183
Kirill <kupuk132@gmail.com>
2020-10-25 10:19:22 +0300

    install spotify api

---
+++ src/spotifyApi/init.js
@@ -0,0 +1,12 @@
+const Spotify = require('spotify-web-api-js')
+const instance = new Spotify()
+
+function getAccessToken() {
+  if (!window.location.hash) return
+  const r = new RegExp('([^&;=]+)=?([^&;]*)', 'g')
+  return window.location.hash.match(r)[0].split('#access_token=')[1]
+}
+
+instance.setAccessToken(getAccessToken())
+
+export default instance
