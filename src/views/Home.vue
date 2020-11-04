0a42ba27ac19423c66ed9b56f3eaae497c6a6fab
Kirill <kupuk132@gmail.com>
2020-11-04 14:09:45 +0300

    fix spotify api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,24 @@
   <div class="home">
     <h1>Sound app</h1>
     <p>Wrapper for soundCloud for listen music</p>
+    <div v-if="!loader">{{playLists}}</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import spotify from '../spotifyApi/index'
 export default {
   name: "Home",
+  data: () => ({
+    playLists: null,
+    loader: true
+  }),
   components: {},
+  async mounted() { 
+    this.playLists = await spotify.getUserPlaylists().then(res => resy);
+    console.log(this.playLists)
+    this.loader = false;
+  }
 };
 </script>
